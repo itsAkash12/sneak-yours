5270b976f74379e4eb6aab0829e0633221efaf50
Akash Kumar Singh <akashviratsingh018@gmail.com>
2023-01-31 05:32:20 +0530

    added:login route with token

--- backend/routes/users.route.js
+++ backend/routes/users.route.js
@@ -1,22 +1,50 @@
+require("dotenv").config();
 const { Router } = require("express");
 const argon2 = require("argon2")
+const jwt = require("jsonwebtoken")
 const UserModel = require("../models/users.model")
 const users = Router();
 
-users.get("/", (req, res) => {
-  res.send("This is users route");
+users.get("/", async(req, res) => {
+    try {
+        const user = await UserModel.find();
+        res.send(user);
+    } catch (error) {
+        res.send(error.message)
+    }
 });
 
-users.post("/signup", async(req,res)=> {
+users.post("/register", async(req,res)=> {
     const {name, email, password} = req.body;
     const hashPassword = await argon2.hash(password)
     try {
         const user = new UserModel({name,email,password:hashPassword});
         await user.save();
-        res.send({message:"Signup Sucessfully"})
+        res.send({message:"Signup Successfully"})
     } catch (error) {
         res.send(error);
-        console.log("error while signup")
+    }
+})
+
+users.post("/login", async(req,res)=> {
+    const {email, password} = req.body;
+    try {
+        const user = await UserModel.findOne({email});
+        if(user){
+            const hashPassword = user.password;
+            const decodedPassword = await argon2.verify(hashPassword, password)
+            if(decodedPassword){
+                const details = {id:user._id, name:user.name, email:user.email, role:user.role}
+                const token = jwt.sign(details, process.env.SECRET_KEY, {expiresIn:"2 days"})
+                return res.send({message:"Logged In Successfully", token})
+            }else{
+                return res.send({message:"Invalid User Credentials"})
+            }
+        }else{
+            res.send({message:"Invalid User Credentials"})
+        }
+    } catch (error) {
+        res.send(error.message);
     }
 })
 
