639ded9b0857886780418b49116ab82014baf013
Akash Kumar Singh <akashviratsingh018@gmail.com>
2023-02-07 20:31:12 +0530

    redeploy: commiting the current changes for deploying

---
+++ frontend/src/pages/Productspage.jsx
@@ -0,0 +1,13 @@
+import { Box } from '@chakra-ui/react'
+import React from 'react'
+import Navbar from '../components/Navbar/Navbar'
+
+const Productspage = () => {
+  return (
+    <Box>
+      <Navbar />
+    </Box>
+  )
+}
+
+export default Productspage
--- frontend/src/routes/Allroutes.jsx
+++ frontend/src/routes/Allroutes.jsx
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom";
 import Navbar from "../components/Navbar/Navbar";
 import Homepage from "../pages/Homepage";
 import Login from "../pages/Login";
+import Productspage from "../pages/Productspage";
 import Signup from "../pages/Signup";
 
 const Allroutes = () => {
@@ -13,6 +14,7 @@ const Allroutes = () => {
         <Route path="/" element={<Homepage />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/products" element={<Productspage />} />
       </Routes>
     </Box>
   );
