931bf7110b0cbcfec98350af1de77225f90b0664
Akash Kumar Singh <akashviratsingh018@gmail.com>
2023-02-20 18:16:47 +0530

    updated: some changes in cart add route

--- backend/controllers/cartsController/addCart.controller.js
+++ backend/controllers/cartsController/addCart.controller.js
@@ -5,22 +5,25 @@ const addCart = async (req, res) => {
   const userId = req.userId;
   const prodId = req.params.prodId;
   try {
-    const product = await ProductModel.findById({_id:prodId});
+    const product = await ProductModel.findById({ _id: prodId });
     if (!product) {
       return res.status(404).send({
         message: "failed",
         description: "Can't find product",
       });
     }
-    const isCartExist = await CartModel.findOne({prodId, userId}) 
-    if(!isCartExist){
-        const cart = await CartModel.create({prodId, userId})
-        return res.send(cart);
-    }else{
-        return res.send({
-            message: "failed",
-            description: "Product is already there",
-        })
+    const isCartExist = await CartModel.findOne({ prodId, userId });
+    if (!isCartExist) {
+      const cart = await CartModel.create({ prodId, userId });
+      return res.send({
+        message: "success",
+        description: "Product added to the cart",
+      });
+    } else {
+      return res.send({
+        message: "failed",
+        description: "Product is already there",
+      });
     }
   } catch (error) {
     res.send({
