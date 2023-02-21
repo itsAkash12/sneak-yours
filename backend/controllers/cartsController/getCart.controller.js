1691bcf481011288169cd0cc3a9f7145527c748c
Akash Kumar Singh <akashviratsingh018@gmail.com>
2023-02-21 16:15:47 +0530

    added: cart count from backend for implementation

--- backend/controllers/cartsController/getCart.controller.js
+++ backend/controllers/cartsController/getCart.controller.js
@@ -4,8 +4,9 @@ const getCarts = async (req, res) => {
     const userId = req.userId;
     try {
       const cart = await CartModel.find({userId}).populate(["prodId"]);
+      const cartCount = await CartModel.find({userId}).countDocuments();
       if(cart.length > 0){
-        return res.send(cart);
+        return res.send({cart, cartCount});
       }
       return res.send({
         message: "failed",
